8c28c9ec4f340e8b68318b60e199f9e807b3e301
yalizada <37800222+yalizada@users.noreply.github.com>
2018-11-13 15:16:52 -0800

    Name

--- DevelopiaSolutions/src/main/java/solutions/beans/Kid.java
+++ DevelopiaSolutions/src/main/java/solutions/beans/Kid.java
@@ -1,6 +1,15 @@
 package solutions.beans;
 
 public class Kid {
+private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 
     private int id;
 
